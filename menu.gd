12ce89508bdc9a10118a723cf02c805366856123
Nicolas Oddo <167439680+Nioddo@users.noreply.github.com>
2026-03-23 14:19:56 -0300

    creamos menu sin conexion

---
+++ menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_facil_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_dificil_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_1v1_pressed() -> void:
+	pass # Replace with function body.
